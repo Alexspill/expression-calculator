25d62839cef34e9721f94903c7113684608b53e3
Alexspill <almi78@mail.ru>
2021-08-30 19:08:19 +0500

    last

--- src/index.js
+++ src/index.js
@@ -4,24 +4,53 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    let exprn = [];
-    let lastSymbol = '';
-    expr.split('').reduce((lastSymbol, item) => {
-        if (item !== ('+' || '-' || '*' || '/' || '(' || ')' || ' ')){
-            lastSymbol = lastSymbol + item;
-            console.log(1, lastSymbol, item);
-        }  else if (item !== ' ') {            
-            exprn.push(lastSymbol);
-            exprn.push(item);
-            lastSymbol = '';
-        }
-        else {
-            exprn.push(lastSymbol);
-            lastSymbol = '';
-        }
-    });
 
-    let tempNumber = 0;
+    let exprX = expr.split(' ').join('').split('');
+    let lastSymbol = '';
+    let stack = [];
+    let result = [];
+    const actions = {
+        '+' : (a, b) => (b + a),
+        '-' : (a, b) => (b - a),
+        '*' : (a, b) => (b * a),
+        '/' : (a, b) => (b / a),  
+    }
+    exprX.forEach(item => {
+        
+    });
+}
+
+function identSymbol(lastSymbol, symbol){
+    if (isAction(symbol)) {
+
+
+
+        lastSymbol += symbol;
+        lastSymbol = lastSymbol + symbol;
+}
+
+function isAction(symbol) {
+    return (symbol === ('+' || '-' || '*' || '/' || '(' || ')' || ' '));
+}
+
+
+
+
+    // expr.split('').reduce((lastSymbol, item) => {
+    //     if (item !== ('+' || '-' || '*' || '/' || '(' || ')' || ' ')) {
+    //         lastSymbol = lastSymbol + item;
+    //         console.log(1, lastSymbol, item, exprn);
+    //     }  else if (item !== ' ') {            
+    //         exprn.push(lastSymbol);
+    //         exprn.push(item);
+    //         lastSymbol = '';
+    //     }
+    //     else {
+    //         exprn.push(item);
+    //         lastSymbol = '';
+    //     }
+    // });
+
     let result = [];
     let itog = [];
     let stack = [];
@@ -33,44 +62,42 @@ function expressionCalculator(expr) {
         '*' : (a, b) => (b * a),
         '/' : (a, b) => (b / a),  
     }
-    // exprn.forEach(el => {
-    //     console.log(typeof el);
-    //     if (typeof el === "number") {
-    //         result.push(el);
-    //     }
-    //     else if (el === '(') {
-    //         stack.push(el);
-    //     }
-    //     return console.log(result, stack);
-    //     else if (el === ')') {
-    //         // lastOpenBracket = stack.lastIndexOf('(');
-    //         for (let i=stack.length; i >= stack.lastIndexOf('('); i--) {
-    //             if (i !== stack.lastIndexOf('(')) {
-    //                 result.push(stack.pop());
-    //                 }
-    //                 else {
-    //                     stack.pop();
-    //                     if (stack[stack.length - 1] === '+' || '-' || '*' || '/') {
-    //                         result.push(stack[stack.length - 1]);
-    //                     }
-    //                 }  
-    //             }          
-    //         }
-    //     else if(el === '+' || '-' || '*' || '/') {
-    //         if (el === '*' || '/') {
-    //             while(stack[stack.length - 1] !== '(') {
-    //                 result.push(stack.pop());
-    //             }
-    //             stack.push(el);
-    //         }
-    //         else {
-    //             while(stack[stack.length - 1] !== '(' || '*' || '/') {
-    //                 result.push(stack.pop());
-    //             }
-    //             stack.push(el);
-    //         }
-    //     }
-    // });
+    exprn.forEach((el) => {
+        if (el !== ('+' || '-' || '*' || '/' || '(' || ')' || ' ')) {
+            result.push(el);
+        }
+        else if (el === '(') {
+            stack.push(el);
+        }
+        else if (el === ')') {
+
+            for (let i=stack.length; i >= stack.lastIndexOf('('); i--) {
+                if (i !== stack.lastIndexOf('(')) {
+                    result.push(stack.pop());
+                    }
+                    else {
+                        stack.pop();
+                        if (stack[stack.length - 1] === '+' || '-' || '*' || '/') {
+                            result.push(stack[stack.length - 1]);
+                        }
+                    }  
+                }          
+            }
+        else if(el === '+' || '-' || '*' || '/') {
+            if (el === '*' || '/') {
+                while(stack[stack.length - 1] !== '(') {
+                    result.push(stack.pop());
+                }
+                stack.push(el);
+            }
+            else {
+                while(stack[stack.length - 1] !== '(' || '*' || '/') {
+                    result.push(stack.pop());
+                }
+                stack.push(el);
+            }
+        }
+    });
 
     // while(stack.length > 0){
     //     result.push(stack.pop());
@@ -85,6 +112,7 @@ function expressionCalculator(expr) {
     //     }
     // })
     // return itog;
+
 }
 
 
